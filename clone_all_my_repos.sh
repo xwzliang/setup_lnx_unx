4f0976ae517c4cf8feae1297f608e3cf6fb5f6a8
Jolong <18706703437@163.com>
2019-09-30 10:59:07 +0800

    New clone all my repos

---
+++ clone_all_my_repos.sh
@@ -0,0 +1,12 @@
+git_path=$HOME/git
+mkdir -p $git_path
+cd $git_path
+
+CNTX=users  # CNTX should be users or orgs (for organization)
+NAME=xwzliang
+PAGE=1  # page number
+
+curl "https://api.github.com/$CNTX/$NAME/repos?page=$PAGE&per_page=100" |
+  grep -e 'git_url*' |
+  cut -d \" -f 4 |
+  xargs -L1 git clone
