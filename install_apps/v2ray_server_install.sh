d62b9834c230f1482e09d5ea70a32d2346c22b18
broliang <18706703437@163.com>
2019-10-02 10:26:31 +0800

    Modify install_apps/v2ray_server_install.sh: Add time calibration

--- install_apps/v2ray_server_install.sh
+++ install_apps/v2ray_server_install.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# Time calibration
+# Check time: date -R
+cp /usr/share/zoneinfo/Asia/Shanghai /etc/localtime 
+
 bash <(curl -L -s https://install.direct/go.sh)
 
-# systemctl start v2ray
+systemctl start v2ray
