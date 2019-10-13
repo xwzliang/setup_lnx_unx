ada686be8205ddfa4e65864b59f6b0d42e026cde
broliang <18706703437@163.com>
2019-10-13 22:10:28 +0800

    Modify privoxy_setup.sh: Only append socks5 setting if not already exists

--- privoxy_setup.sh
+++ privoxy_setup.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
-echo 'forward-socks5 / 127.0.0.1:1080 .' >>/etc/privoxy/config
+socks5_setting='forward-socks5 / 127.0.0.1:1080 .'
+privoxy_config=/etc/privoxy/config
+
+if ! grep -q "$socks5_setting" "$privoxy_config"; then
+	echo $socks5_setting >> $privoxy_config
+fi
 
 systemctl start privoxy.service
-systemctl -l status privoxy.service
+# systemctl -l status privoxy.service
