0fdd19d9a7623c1d678105664888a7cb3d0521ac
broliang <18706703437@163.com>
2019-10-14 11:46:47 +0800

    Revert vim_setup.sh: Revert the last modification, the ft will overwrite filetype set by setfiletype

--- vim_setup.sh
+++ vim_setup.sh
@@ -12,7 +12,6 @@ endif
 augroup filetypedetect
   au! BufRead,BufNewFile *.sh		setfiletype bash | set syntax=sh
   au! BufRead,BufNewFile *.py		setfiletype python3 | set syntax=python
-  " let is_bash=1 and set ft=sh to tell vim to use bash highlight mode instead of multipurpose default (sh, bash and ksh)
-  au! BufRead,BufNewFile *.bats		setfiletype bats | set syntax=sh | let is_bash=1 | set ft=sh
+  au! BufRead,BufNewFile *.bats		setfiletype bats | set syntax=sh
 augroup END
 _EOF_
