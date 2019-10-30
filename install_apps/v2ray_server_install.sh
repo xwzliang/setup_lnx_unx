332feb1db9fdc6aa63400f25d73c135f9f2c698f
broliang <18706703437@163.com>
2019-10-30 20:56:22 +0800

    Add v2ray_server_install.sh: Add link for directly install

--- install_apps/v2ray_server_install.sh
+++ install_apps/v2ray_server_install.sh
@@ -7,3 +7,6 @@ cp /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 bash <(curl -L -s https://install.direct/go.sh)
 
 systemctl start v2ray
+
+## To only install this directly, use following:
+# bash <(curl -L -s https://raw.githubusercontent.com/xwzliang/setup_lnx_unx/master/install_apps/v2ray_server_install.sh)
