8795d7bd5b4f37428f2266caca24f015d075bd4f
broliang <18706703437@163.com>
2020-01-29 23:05:12 +0800

    Modify clone_all_my_repos.sh: Do not clone art_replica.

--- clone_all_my_repos.sh
+++ clone_all_my_repos.sh
@@ -11,4 +11,5 @@ PAGE=1  # page number
 curl "https://api.github.com/$CNTX/$NAME/repos?page=$PAGE&per_page=100" |
   grep -e 'clone_url*' |
   cut -d \" -f 4 |
+  grep -v art_replica |
   xargs -L1 git clone
