8323ee6ce89f80e01e640de5360c102127ffc60e
broliang <18706703437@163.com>
2020-01-30 20:53:57 +0800

    Modify privoxy_setup.sh: Add support to MacOS

--- privoxy_setup.sh
+++ privoxy_setup.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/env bash
 
-socks5_setting='forward-socks5 / 127.0.0.1:1080 .'
-privoxy_config=/etc/privoxy/config
-
-if ! grep -q "$socks5_setting" "$privoxy_config"; then
-	echo $socks5_setting >> $privoxy_config
+if [ "$(uname)" == "Darwin" ]; then
+	# MacOS
+	privoxy_config=/usr/local/etc/privoxy/config
+	socks5_setting='forward-socks5 / localhost:1086 .'
+	if ! grep -q "$socks5_setting" "$privoxy_config"; then
+		echo $socks5_setting >> $privoxy_config
+	fi
+	brew services start privoxy
+else
+	privoxy_config=/etc/privoxy/config
+	socks5_setting='forward-socks5 / 127.0.0.1:1080 .'
+	if ! grep -q "$socks5_setting" "$privoxy_config"; then
+		echo $socks5_setting >> $privoxy_config
+	fi
+	systemctl start privoxy.service
+	# systemctl -l status privoxy.service
 fi
-
-systemctl start privoxy.service
-# systemctl -l status privoxy.service
