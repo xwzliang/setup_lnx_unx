12fc7a76a1606a9d9493c4f636dd1e8cf1834dd7
broliang <18706703437@163.com>
2020-01-31 17:42:13 +0800

    Delete setup_Ubuntu_Mac.sh: Remove emms from repos_git_clone list.

--- setup_Ubuntu_Mac.sh
+++ setup_Ubuntu_Mac.sh
@@ -127,7 +127,6 @@ declare -A repos_git_clone
 repos_git_clone=(
 	["$HOME/.tmux/plugins/tpm"]="https://github.com/tmux-plugins/tpm"
 	["$HOME/.emacs.d/evil"]="https://github.com/emacs-evil/evil"
-	["$HOME/.emacs.d/emms"]="git://git.sv.gnu.org/emms.git"
 )
 
 for directory_git_local in "${!repos_git_clone[@]}"; do
