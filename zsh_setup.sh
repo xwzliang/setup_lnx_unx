f8025513c71245ff58d1277e1f650216183f6586
broliang <18706703437@163.com>
2020-02-05 12:25:59 +0800

    Add zsh_setup.sh: Source custom env settings file .all_sh_env in .zshenv.

--- zsh_setup.sh
+++ zsh_setup.sh
@@ -3,3 +3,7 @@
 if ! grep -h bash_aliases $HOME/.zshrc; then
 	echo "source $HOME/.bash_aliases" >> $HOME/.zshrc
 fi
+
+if ! grep -h all_sh_env $HOME/.zshenv; then
+	echo "source $HOME/.all_sh_env" >> $HOME/.zshenv
+fi
