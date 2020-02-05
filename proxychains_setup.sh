0179add43f292200bf2440d290c29cd75b725882
broliang <18706703437@163.com>
2020-02-05 10:42:45 +0800

    Modify proxychains_setup.sh: Add support to MacOS.

--- proxychains_setup.sh
+++ proxychains_setup.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 
-proxychains_conf=/etc/proxychains.conf
-proxychains_conf_bak=/etc/proxychains.conf.bak
+if [ "$(uname)" == "Darwin" ]; then
+	# MacOS
+	proxychains_conf=/usr/local/etc/proxychains.conf
+	listen_port=1086
+else
+	# Ubuntu
+	proxychains_conf=/etc/proxychains.conf
+	listen_port=1080
+fi
+
+proxychains_conf_bak=$proxychains_conf.bak
 
 if [ ! -e $proxychains_conf_bak ]; then
 	cp $proxychains_conf $proxychains_conf_bak
@@ -9,7 +18,8 @@ fi
 
 if [[ "`tail -2 $proxychains_conf`" =~ ^socks4.* ]]; then
 	# change the second last line using tac and sed
-	tac $proxychains_conf_bak | sed '2s/^.*$/socks5 127.0.0.1 1080/' | tac > $proxychains_conf
+	tac $proxychains_conf_bak | sed '2d' | tac > $proxychains_conf
+	echo "socks5 127.0.0.1 $listen_port/" >> $proxychains_conf
 fi
 
 # To test, run:
