72b5ca57316b539119873e0c9b4999ac6cc7897a
broliang <18706703437@163.com>
2020-02-18 16:05:48 +0800

    New install_apps/mac_brew_cask_install.sh

---
+++ install_apps/mac_brew_cask_install.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+apps_to_install_brew_cask=(
+	basictex
+)
+
+for app in "${apps_to_install_brew_cask[@]}"; do
+	if brew cask ls --versions ${app} > /dev/null 2>&1; then
+		echo -e "${app} already installed, will skip it"
+	else
+		if brew cask info ${app} > /dev/null 2>&1 ; then
+			echo -e "${app} not installed, will install it\n"
+			HOMEBREW_NO_AUTO_UPDATE=1 brew cask install ${app}
+		else
+			echo "${app} is not available in homebrew."
+		fi
+	fi
+done
