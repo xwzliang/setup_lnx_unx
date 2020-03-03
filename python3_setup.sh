88d3c6443585286639c1fee5995fbefecdc42312
broliang <18706703437@163.com>
2020-03-03 18:23:04 +0800

    Add python3_setup.sh: Add isbnlib to pip install list.

--- python3_setup.sh
+++ python3_setup.sh
@@ -4,10 +4,11 @@ modules_to_install=(
 	numpy
 	pandas
 	pyzotero
+	isbnlib		# isbnlib is a (pure) python library that provides several useful methods and functions to validate, clean, transform, hyphenate and get metadata for ISBN strings.
 )
 
 for module in "${modules_to_install[@]}"; do
-	if python3 -c "import $module" > /dev/null; then
+	if python3 -c "import $module" > /dev/null 2>&1; then
 		echo "$module already installed."
 	else
 		pip3 install $module
