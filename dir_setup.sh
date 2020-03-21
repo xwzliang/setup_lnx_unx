7fff606c5eb6cb8278084aa13ca7bdfe2637bc5d
broliang <18706703437@163.com>
2020-03-21 18:43:05 +0800

    Add dir_setup.sh: create some files as well.

--- dir_setup.sh
+++ dir_setup.sh
@@ -16,3 +16,12 @@ my_dirs=(
 for my_dir in "${my_dirs[@]}"; do
 	mkdir -p $my_dir
 done
+
+my_files=(
+	~/pipes/zotero/linked_url
+	~/pipes/zotero/amazon_book.html
+)
+
+for my_file in "${my_files[@]}"; do
+	touch $my_file
+done
