806f27ffd0d7a4b7096c07e6eb4efa5566e0b57b
broliang <18706703437@163.com>
2020-04-06 11:32:52 +0800

    Add bash_setup.sh: Add set up for z.

--- bash_setup.sh
+++ bash_setup.sh
@@ -3,3 +3,16 @@
 if ! grep -h bash_aliases $HOME/.bashrc; then
 	echo "source $HOME/.bash_aliases" >> $HOME/.bashrc
 fi
+
+# z set up: jump around easily
+if [ "$(uname)" == "Darwin" ]; then
+	# MacOS
+	z_path=/usr/local/etc/bash_completion.d/z.bash
+else
+	# Ubuntu
+	z_path=/etc/bash_completion.d/z.bash
+fi
+
+if [ ! -f $z_path ]; then
+	sudo wget -O $z_path https://raw.githubusercontent.com/rupa/z/master/z.sh
+fi
