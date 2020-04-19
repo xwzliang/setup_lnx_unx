5079fee1a277089caf66ea36abeb357e65e08f84
broliang <18706703437@163.com>
2020-04-19 18:22:58 +0800

    Add python3_setup.sh: Add more packages to install list.

--- python3_setup.sh
+++ python3_setup.sh
@@ -10,6 +10,9 @@ modules_to_install=(
 	jedi				# Jedi is a static analysis tool for Python that is typically used in IDEs/editors plugins. Jedi has a focus on autocompletion and goto functionality.
 	rope				# a python refactoring library
 	trepan3k			# A gdb-like Python3 Debugger in the Trepan family
+	black				# The uncompromising code formatter
+	isort				# A Python utility / library to sort imports 
+	flake8				# flake8 is a python tool that glues together pep8, pyflakes, mccabe, and third-party plugins to check the style and quality of some python code
 )
 
 for module in "${modules_to_install[@]}"; do
