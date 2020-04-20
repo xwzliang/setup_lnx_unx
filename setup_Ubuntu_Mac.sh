277415dfac506999e74d61948551e879e377e67c
broliang <18706703437@163.com>
2020-04-20 12:48:48 +0800

    Rename: Restructure directories.

--- setup_Ubuntu_Mac.sh
+++ setup_Ubuntu_Mac.sh
@@ -1,202 +1,44 @@
 #!/usr/bin/env bash
 
-# Add ppa repos
-# ppa_to_add=(
-# )
-# for app in "${ppa_to_add[@]}"; do
-	# if ! grep -q "^deb .*${app}" /etc/apt/sources.list /etc/apt/sources.list.d/*; then
-		# # commands to add the ppa ...
-		# add-apt-repository ppa:${app} -y
-	# fi
-# done
-
-# apt-get update
-
-apps_to_install=(
-	git
-	curl
-	vim
-	emacs
-	python3
-	tmux
-	aria2
-	pandoc
-	w3m
-	imagemagick
-	ffmpeg
-	mosh
-	ocrmypdf
-	rename
-	elinks      # Full-Featured Text web Browser, advanced and well-established feature-rich text mode web browser
-	shadowsocks-libev
-	privoxy
-	expect      # a language that talks with your interactive programs or scripts that require user interaction
-	bats        # Bash Automated Testing System
-	check		# A unit testing framework for C
-	jq          # jq is like sed for JSON data - you can use it to slice and filter and map and transform structured data
-	unrar       # For extracting rar archive files
-	global      # Source code tag system
-	graphviz	# open source graph visualization software. Graph visualization is a way of representing structural information as diagrams of abstract graphs and networks
-	fzf			# fzf is a general-purpose command-line fuzzy finder.
-	ack			# Search tool like grep, but optimized for programmers
-	git-flow			# Git extensions to provide high-level repository operations for Vincent Driessen's branching model
-	bash-completion			# Programmable completion functions for bash
-	ripgrep			# ripgrep recursively searches directories for a regex pattern (faster than ag)
-	zsh			# Zsh is a shell designed for interactive use, although it is also a powerful scripting language
-)
-
-if [ "$(uname)" == "Darwin" ]; then
-	# MacOS
-	apps_to_install+=( 
-		bash
-		coreutils
-		grep
-		gnu-sed
-		gawk
-		findutils
-		gnu-tar
-		autoconf
-		automake
-		docker
-		youtube-dl
-		proxychains-ng
-		aspell
-		poppler                 # PDF rendering library
-		gdb                     # GNU debugger
-		gcc                     # GNU compiler collection
-		ncurses                 # The ncurses library routines are a terminal-independent method of updating character screens with reasonable optimization. 
-		the_silver_searcher		# A code-searching tool similar to ack, but faster
-		readline				# GNU readline and history libraries
-		pyenv					# Simple Python version management
-	)
-	for app in "${apps_to_install[@]}"; do
-		if brew ls --versions ${app} > /dev/null; then
-			echo -e "${app} already installed, will skip it"
-		else
-			if brew info ${app} > /dev/null 2>&1 ; then
-				echo -e "${app} not installed, will install it\n"
-				HOMEBREW_NO_AUTO_UPDATE=1 brew install ${app}
-			else
-				echo "${app} is not available in homebrew."
-			fi
-		fi
-	done
-else
-	# Ubuntu
-	apps_to_install+=( 
-		openssh-server          # A powerful collection of tools for remote control
-		net-tools
-		bcmwl-kernel-source		# Wifi adapter for laptop
-		python3-pip
-		djvulibre-bin           # djvulibre including ddjvu
-		calibre
-		xterm
-		mpv
-		hfsprogs
-		python3-lxml
-		proxychains
-		virtualbox              # A general-purpose full virtualizer
-		vagrant                 # A tool for building and managing virtual machine environments in a single workflow
-		ruby-full
-		glimpse                 # A very popular UNIX indexing and query system to search quickly through entire file systems, including agrep
-		valgrind                # A tool for finding memory access errors to heap memory in C and C++ programs
-		poppler-utils           # PDF rendering library
-		anki                    # Anki is a program designed to help you remember facts as easily, quickly and efficiently as possible
-		apt-file                # apt-file is a command line tool for searching files contained in packages for the APT packaging system. You can search in which package a file is included or list the contents of a package without installing or fetching it.
-		libncurses-dev          # The ncurses library routines are a terminal-independent method of updating character screens with reasonable optimization. 
-		silversearcher-ag		# A code-searching tool similar to ack, but faster
-		libbz2-dev              # bzip2 compressor library
-		libreadline-dev         # GNU readline and history libraries
-		libsqlite3-dev          # SQLite 3 development files
-	)
-	for app in "${apps_to_install[@]}"; do
-		PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $app | grep "install ok installed")
-		if [ "" == "$PKG_OK" ]; then
-			echo -e "${app} not installed, will install it\n"
-			sudo apt install -y ${app}
-		else
-			echo -e "${app} already installed, will skip it"
-		fi
-	done
-fi
-
-# Install apps using other methods
-
-command_exists() {
-	command -v "$@" > /dev/null 2>&1
-}
-
-declare -A apps_other_methods
-
-apps_other_methods=(
-	["docker"]="wget -qO- https://get.docker.com/ | sh"
-	["youtube-dl"]="curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl && chmod a+rx /usr/local/bin/youtube-dl"
-	["ceedling"]="gem install ceedling"		# Ceedling is an automated testing framework for C applications. 
-	["pyenv"]="curl_socks -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash"		# Simple Python version management
-)
-
-for app_other in "${!apps_other_methods[@]}"; do
-	if ! command_exists $app_other; then
-		echo -e "${app_other} not installed, will install it\n"
-		eval ${apps_other_methods[$app_other]}
-	else
-		echo -e "${app_other} already installed, will skip it"
-	fi
-done
-
-
-# Clone repository using git
-
-declare -A repos_git_clone
-
-repos_git_clone=(
-	["$HOME/.tmux/plugins/tpm"]="https://github.com/tmux-plugins/tpm"
-)
-
-for directory_git_local in "${!repos_git_clone[@]}"; do
-	if [ ! -d $directory_git_local ]; then
-		echo -e "${directory_git_local} not exists, will clone it using git\n"
-		git clone ${repos_git_clone[$directory_git_local]} $directory_git_local
-	else
-		echo -e "${directory_git_local} already exists, will skip it"
-	fi
-done
-
-
 dir="$(dirname "$0")"
+install_dir=$dir/install_packages
+setup_dir=$dir/setup_packages
+
+# Install packages
+$install_dir/system_packages_install.sh
+
+# Python3 install packages
+$install_dir/python3_modules_install.sh
+
+# Emacs install packages
+$install_dir/emacs_packages_install.sh
 
 # directory setup
-$dir/dir_setup.sh
+$setup_dir/dir_setup.sh
 
 # bash setup
-$dir/bash_setup.sh
+$setup_dir/bash_setup.sh
 
 # zsh setup
-$dir/zsh_setup.sh
+$setup_dir/zsh_setup.sh
 
 # git credential settings, import setting_git_credential.sh
-$dir/git_setup.sh
+$setup_dir/git_setup.sh
 
 # vim settings
-$dir/vim_setup.sh
-
-# emacs settings
-$dir/emacs_setup.sh
+$setup_dir/vim_setup.sh
 
 # proxychains settings
-$dir/proxychains_setup.sh
+$setup_dir/proxychains_setup.sh
 
 # privoxy settings
-$dir/privoxy_setup.sh
-
-# Python3 settings
-$dir/python3_setup.sh
+$setup_dir/privoxy_setup.sh
 
 if [ "$(uname)" == "Darwin" ]; then
 	# MacOS settings
-	$dir/macos_setup.sh
+	$setup_dir/macos_setup.sh
 else
 	# systemd settings
-	# $dir/systemd_setup.sh
+	# $setup_dir/systemd_setup.sh
 	echo ubuntu
 fi
--- setup_packages/bash_setup.sh
+++ setup_packages/bash_setup.sh
@@ -20,14 +20,3 @@ fi
 if [ ! -f $z_path ]; then
 	sudo wget -O $z_path https://raw.githubusercontent.com/rupa/z/master/z.sh
 fi
-
-command_exists() {
-	command -v "$@" > /dev/null 2>&1
-}
-
-dir="$(dirname "$0")"
-
-# Install bashdb if not already
-if ! command_exists bashdb; then
-	$dir/install_apps/bashdb_install.sh
-fi
