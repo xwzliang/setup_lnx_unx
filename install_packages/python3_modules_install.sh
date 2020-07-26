018e78cef3ae0540b790617f6514ebd573947249
broliang <18706703437@163.com>
2020-07-26 23:44:28 +0800

    Add install_packages/python3_modules_install.sh: Add mrjob and pyspark to install.

--- install_packages/python3_modules_install.sh
+++ install_packages/python3_modules_install.sh
@@ -15,6 +15,8 @@ modules_to_install=(
     flake8				# flake8 is a python tool that glues together pep8, pyflakes, mccabe, and third-party plugins to check the style and quality of some python code
     docker-compose				# Define and run multi-container applications with Docker
     lastversion					# Fetch latest release version of an arbitrary project
+    mrjob						# mrjob is a Python 2.7/3.4+ package that helps you write and run Hadoop Streaming jobs.
+    pyspark						# Python API for Spark
 )
 
 for module in "${modules_to_install[@]}"; do
