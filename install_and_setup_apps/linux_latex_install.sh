9a02c7be5b11a598e18f59385191e93adde9487d
broliang <18706703437@163.com>
2020-08-05 19:26:47 +0800

    New install_and_setup_apps/linux_latex_install.sh

---
+++ install_and_setup_apps/linux_latex_install.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+apps_to_install=(
+    texlive-latex-base		# TeX Live: LaTeX fundamental packages, including pdflatex
+    texlive-latex-extra		# TeX Live: LaTeX additional packages
+    texlive-fonts-recommended
+    # texlive-fonts-extra		# large font package
+    texlive-lang-italian	# For LaTeX Error: File `layaureo.sty' not found.
+)
+
+for app in "${apps_to_install[@]}"; do
+    PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $app | grep "install ok installed")
+    if [ "" == "$PKG_OK" ]; then
+        echo -e "${app} not installed, will install it\n"
+        sudo apt install -y ${app}
+    else
+        echo -e "${app} already installed, will skip it"
+    fi
+done
