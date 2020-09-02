2ce9f8f1a12d7318fbda921c7d4fc423bddff026
broliang <18706703437@163.com>
2020-09-02 09:22:32 +0800

    New install_and_setup_apps/google_chrome_install.sh

---
+++ install_and_setup_apps/google_chrome_install.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+cd ~/Downloads/bin
+
+curl_socks -O https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+
+sudo apt install ./google-chrome-stable_current_amd64.deb
