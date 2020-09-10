df0bd5ed571ac28515b835891b959cc6333b865a
Jolong <18706703437@163.com>
2020-09-10 16:18:58 +0800

    Update apt_aliyun_mirror.sh

--- setup_packages/apt_aliyun_mirror.sh
+++ setup_packages/apt_aliyun_mirror.sh
@@ -18,3 +18,5 @@ deb http://mirrors.aliyun.com/ubuntu/ bionic-proposed main restricted universe m
 deb http://mirrors.aliyun.com/ubuntu/ bionic-backports main restricted universe multiverse
 #deb-src http://mirrors.aliyun.com/ubuntu/ bionic-backports main restricted universe multiverse
 EOF
+
+sudo apt update
