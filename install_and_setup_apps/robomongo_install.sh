f9883d7d2390efe839d4c15b5979a7888f7b87c1
broliang <18706703437@163.com>
2020-09-30 21:38:51 +0800

    New: install_and_setup_apps/robomongo_install.sh

---
+++ install_and_setup_apps/robomongo_install.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+mkdir -p $HOME/Downloads/bin
+cd $HOME/Downloads/bin
+
+aria2c https://download.studio3t.com/robomongo/linux/studio-3t-robo-3t-linux-double-pack.tar.gz
+
+tar xf studio-3t-robo-3t-linux-double-pack.tar.gz
