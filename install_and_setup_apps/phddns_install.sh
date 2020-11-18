945d78bd055a47eeb00837c81e56644bbca8629c
broliang <18706703437@163.com>
2020-11-18 22:09:05 +0800

    Modify: new version of phddns.

--- install_and_setup_apps/phddns_install.sh
+++ install_and_setup_apps/phddns_install.sh
@@ -3,5 +3,6 @@
 mkdir -p $HOME/Downloads/bin
 cd $HOME/Downloads/bin
 
-aria2c http://download.oray.com/peanuthull/linux/phddns_3.0_x86_64.deb
-dpkg -i phddns*.deb
+# aria2c http://download.oray.com/peanuthull/linux/phddns_3.0_x86_64.deb
+aria2c https://dl-cdn.oray.com/hsk/linux/phddns-5.0.0-amd64.deb
+sudo dpkg -i phddns*.deb
