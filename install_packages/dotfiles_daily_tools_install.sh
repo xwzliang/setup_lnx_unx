53615188b20953f72fe3a181efa92ec3fd6d5a1d
broliang <18706703437@163.com>
2021-05-18 19:20:47 +0800

    Add dot_emacs to setup

--- install_packages/dotfiles_daily_tools_install.sh
+++ install_packages/dotfiles_daily_tools_install.sh
@@ -7,9 +7,13 @@ sudo apt install -y git
 
 git clone https://github.com/xwzliang/dotfiles.git
 git clone https://github.com/xwzliang/daily_tools
+git clone https://github.com/xwzliang/dot_emacs.git
 
 cd ~/git/dotfiles
 ./install.sh
 
 cd ~/git/daily_tools
 ./install.sh
+
+cd ~/git/dot_emacs
+make
