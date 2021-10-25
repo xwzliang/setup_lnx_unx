903589acb7190c8fdc7a91a319d764ab1aa69ac4
broliang <18706703437@163.com>
2021-10-25 16:32:43 +0800

    Improve packages install workflow with different proxy.

--- install_packages/emacs_packages_install.sh
+++ install_packages/emacs_packages_install.sh
@@ -2,4 +2,29 @@
 
 . set_privoxy
 
-yes | emacs --script ~/git/dot_emacs/init.el
+# emacs --batch --eval '(print "working")'
+
+# First use privoxy to bootstrap straight.el
+emacs --batch --eval "$(cat <<'EOF'
+    (progn
+        ;; Bootstrap code for straight.el package manager
+        (defvar bootstrap-version)
+        (let ((bootstrap-file
+            (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+            (bootstrap-version 5))
+        (unless (file-exists-p bootstrap-file)
+            (with-current-buffer
+                (url-retrieve-synchronously
+                "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+                'silent 'inhibit-cookies)
+            (goto-char (point-max))
+            (eval-print-last-sexp)))
+        (load bootstrap-file nil 'nomessage))
+    )
+EOF
+)"
+
+. set_proxy
+# Then use socks proxy to clone git repos
+yes | emacs --batch --script ~/git/dot_emacs/init.el
+
