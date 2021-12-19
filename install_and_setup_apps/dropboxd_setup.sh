ab2275f658bd68d098b35f41d3c81014f3a05027
broliang <18706703437@163.com>
2021-12-19 08:36:47 +0800

    Don't use proxy by default for dropboxd.

--- install_and_setup_apps/dropboxd_setup.sh
+++ install_and_setup_apps/dropboxd_setup.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
 # proxy settings
-dropboxd proxy "manual" "socks5" '127.0.0.1' 1080
+# dropboxd proxy "manual" "socks5" '127.0.0.1' 1080
+
+# proxy unset
+# dropboxd proxy "none"
 
 # Autostart at login
 dropboxd autostart y
