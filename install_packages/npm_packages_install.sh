d6fe0cd6ac7fa66f76fb2cb3f3df8e08b49a23d5
broliang <18706703437@163.com>
2021-12-23 16:38:14 +0800

    Install rollup using npm.

--- install_packages/npm_packages_install.sh
+++ install_packages/npm_packages_install.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 npm_packages_to_install=(
+    rollup								# Rollup is a module bundler for JavaScript which compiles small pieces of code into something larger and more complex, such as a library or application
     webpack								# A bundler for javascript and friends. Packs many modules into a few bundled assets
     webpack-cli							# The official CLI of webpack
     web-ext								# This is a command line tool to help build, run, and test WebExtensions
