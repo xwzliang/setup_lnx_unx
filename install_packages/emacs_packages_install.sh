5c4efda1f9bb74a901c85a767923dcfc24fbf8f4
broliang <18706703437@163.com>
2021-12-30 19:15:42 +0800

    Loop to install emacs package until success.

--- install_packages/emacs_packages_install.sh
+++ install_packages/emacs_packages_install.sh
@@ -1,30 +1,34 @@
 #!/usr/bin/env bash
 
-. set_privoxy
+# . set_privoxy
 
 # emacs --batch --eval '(print "working")'
 
 # First use privoxy to bootstrap straight.el
-emacs --batch --eval "$(cat <<'EOF'
-    (progn
-        ;; Bootstrap code for straight.el package manager
-        (defvar bootstrap-version)
-        (let ((bootstrap-file
-            (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
-            (bootstrap-version 5))
-        (unless (file-exists-p bootstrap-file)
-            (with-current-buffer
-                (url-retrieve-synchronously
-                "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
-                'silent 'inhibit-cookies)
-            (goto-char (point-max))
-            (eval-print-last-sexp)))
-        (load bootstrap-file nil 'nomessage))
-    )
-EOF
-)"
+# emacs --batch --eval "$(cat <<'EOF'
+#     (progn
+#         ;; Bootstrap code for straight.el package manager
+#         (defvar bootstrap-version)
+#         (let ((bootstrap-file
+#             (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+#             (bootstrap-version 5))
+#         (unless (file-exists-p bootstrap-file)
+#             (with-current-buffer
+#                 (url-retrieve-synchronously
+#                 "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+#                 'silent 'inhibit-cookies)
+#             (goto-char (point-max))
+#             (eval-print-last-sexp)))
+#         (load bootstrap-file nil 'nomessage))
+#     )
+# EOF
+# )"
 
-. set_proxy
-# Then use socks proxy to clone git repos
-yes | emacs --batch --script ~/git/dot_emacs/init.el
+# . set_proxy
+# # Then use socks proxy to clone git repos
+# yes | emacs --batch --script ~/git/dot_emacs/init.el
 
+# loop until emacs package installation finished
+until yes | emacs --batch --script ~/git/dot_emacs/init.el; do
+    echo "Retring in 10 seconds"
+done
