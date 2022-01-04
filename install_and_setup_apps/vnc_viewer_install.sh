d3aa64913433bb85b879b3a7c047c34cc217b44f
broliang <18706703437@163.com>
2022-01-04 11:04:32 +0800

    Use newer version of vncserver.

--- install_and_setup_apps/vnc_viewer_install.sh
+++ install_and_setup_apps/vnc_viewer_install.sh
@@ -5,9 +5,10 @@ mkdir -p bin
 cd bin
 
 # client
-aria2c https://www.realvnc.com/download/file/viewer.files/VNC-Viewer-6.19.923-Linux-x64.deb
-dpkg -i VNC-Viewer-6.19.923-Linux-x64.deb
+# aria2c https://www.realvnc.com/download/file/viewer.files/VNC-Viewer-6.19.923-Linux-x64.deb
+# dpkg -i VNC-Viewer-6.19.923-Linux-x64.deb
 
+server_version=6.8.0
 # server
-aria2c https://www.realvnc.com/download/file/vnc.files/VNC-Server-6.6.0-Linux-x64.deb
-dpkg -i VNC-Server-6.6.0-Linux-x64.deb
+aria2c https://www.realvnc.com/download/file/vnc.files/VNC-Server-$server_version-Linux-x64.deb
+sudo dpkg -i VNC-Server-$server_version-Linux-x64.deb
