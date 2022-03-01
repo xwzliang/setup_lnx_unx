191760e885bc720d03d1f34d37dc4adbae69d634
broliang <18706703437@163.com>
2022-03-01 15:11:23 +0800

    Go install hover.

--- install_packages/go_packages_install.sh
+++ install_packages/go_packages_install.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 go_packages_to_install=(
+    github.com/go-flutter-desktop/hover@latest				# Hover is a simple build tool to create Flutter desktop applications
     github.com/lighttiger2505/sqls				# An implementation of the Language Server Protocol for SQL
     github.com/iawia002/annie					# Fast and simple video download library and CLI tool written in Go
     github.com/shuLhan/go-bindata/...			# This package converts any file into managable Go source code. Useful for embedding binary data into a go program
