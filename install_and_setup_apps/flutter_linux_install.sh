172c56981fee83997f527e91a33548dcbb5accc5
broliang <18706703437@163.com>
2022-04-25 15:20:54 +0800

    Don't install android studio by default for linux flutter.

--- install_and_setup_apps/flutter_linux_install.sh
+++ install_and_setup_apps/flutter_linux_install.sh
@@ -3,22 +3,22 @@
 sudo snap install flutter --classic
 
 sudo apt-get install -y clang cmake ninja-build pkg-config libgtk-3-dev liblzma-dev
-sudo apt-get install -y default-jre default-jdk
+# sudo apt-get install -y default-jre default-jdk
 
-# Install Android Studio
-sudo snap install android-studio --classic
+# # Install Android Studio
+# sudo snap install android-studio --classic
 
-flutter config --android-studio-dir /snap/android-studio/current/android-studio
+# flutter config --android-studio-dir /snap/android-studio/current/android-studio
 
-# snap version of sdkmanager
-sudo snap install androidsdk
-androidsdk --install "cmdline-tools;latest"
-$HOME/AndroidSDK/cmdline-tools/latest/bin/sdkmanager --sdk_root=$HOME/Android/Sdk --install "cmdline-tools;latest"
+# # snap version of sdkmanager
+# sudo snap install androidsdk
+# androidsdk --install "cmdline-tools;latest"
+# $HOME/AndroidSDK/cmdline-tools/latest/bin/sdkmanager --sdk_root=$HOME/Android/Sdk --install "cmdline-tools;latest"
 
-### Then open andrioid studio to finish setup and android-sdk downloading.
+# ### Then open andrioid studio to finish setup and android-sdk downloading.
 
-# Accept android licenses
-flutter doctor --android-licenses
+# # Accept android licenses
+# flutter doctor --android-licenses
 
-# Run flutter doctor to see if everything is OK
-flutter doctor
+# # Run flutter doctor to see if everything is OK
+# flutter doctor
