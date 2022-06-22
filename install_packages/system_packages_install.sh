f656cabf4e990a420d61d8c82f48960da4473763
broliang <18706703437@163.com>
2022-06-23 06:19:59 +0800

    Add mpv to install for macos.

--- install_packages/system_packages_install.sh
+++ install_packages/system_packages_install.sh
@@ -39,7 +39,8 @@ apps_to_install=(
     progress				# Linux tool to show progress for cp, mv, dd, ...
     sox           # Swiss army knife of sound processing
     libsox-fmt-mp3            # SoX MP2 and MP3 format library
-    mplayer			# video and audio player for Unix-like systems
+    # mplayer			# video and audio player for Unix-like systems
+    mpv
     mosh
     ocrmypdf
     rename
@@ -160,7 +161,7 @@ else
         djvulibre-bin           # djvulibre including ddjvu
         # calibre
         xterm
-        mpv
+        # mpv
         hfsprogs
         python3-lxml
         proxychains
