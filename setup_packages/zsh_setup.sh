cf1c9fed1bb9076ad6b20c2a2397d30c569c6771
broliang <18706703437@163.com>
2022-07-04 03:09:03 -0400

    Don't use socks for zsh setup by default.

--- setup_packages/zsh_setup.sh
+++ setup_packages/zsh_setup.sh
@@ -9,10 +9,11 @@ fi
 
 # Install ohmyzsh if not already installed
 if [ ! -d ~/.oh-my-zsh ]; then
-    # sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    echo "not installed"
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
     # Using socks proxy
-    . set_proxy
-    sh -c "$(curl_socks -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    # . set_proxy
+    # sh -c "$(curl_socks -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
 
 # Clone plugins repository using git
