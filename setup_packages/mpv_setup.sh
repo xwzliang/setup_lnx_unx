45d4b60da20661cf015c7cd3f1decfe09fc1f18c
broliang <18706703437@163.com>
2022-07-04 03:09:22 -0400

    New setup_packages/mpv_setup.sh

---
+++ setup_packages/mpv_setup.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+mkdir -p $HOME/.config/mpv/scripts
+
+cd $HOME/.config/mpv/scripts
+wget https://gist.githubusercontent.com/CyberShadow/2f71a97fb85ed42146f6d9f522bc34ef/raw/744c3ee61d2f0a8e9bb4e308dec6897215ae4704/autosave.lua
