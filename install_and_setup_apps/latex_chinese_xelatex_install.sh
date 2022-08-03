647190cea9a669916fc05ced09c2b9fcbf3556da
broliang <18706703437@163.com>
2022-08-03 18:39:20 +0800

    New install_and_setup_apps/latex_chinese_xelatex_install.sh

---
+++ install_and_setup_apps/latex_chinese_xelatex_install.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+sudo apt-get install texlive-xetex
+
+# Install font
+sudo apt-get install ttf-wqy-zenhei
+
+# In tex file, use this setup
+# \usepackage{xeCJK}
+# \setCJKmainfont{WenQuanYi Zen Hei}
