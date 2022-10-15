1100c7291e173704fd2dec5033bbf36bee7d685f
broliang <18706703437@163.com>
2022-10-15 17:20:53 +0800

    Install lux using go.

--- install_packages/go_packages_install.sh
+++ install_packages/go_packages_install.sh
@@ -2,14 +2,16 @@
 
 go_packages_to_install=(
     github.com/go-flutter-desktop/hover@latest				# Hover is a simple build tool to create Flutter desktop applications
-    github.com/lighttiger2505/sqls				# An implementation of the Language Server Protocol for SQL
-    github.com/iawia002/annie					# Fast and simple video download library and CLI tool written in Go
-    github.com/shuLhan/go-bindata/...			# This package converts any file into managable Go source code. Useful for embedding binary data into a go program
+    github.com/lighttiger2505/sqls@latest				# An implementation of the Language Server Protocol for SQL
+    # github.com/iawia002/annie@latest					# Fast and simple video download library and CLI tool written in Go
+    github.com/iawia002/lux@latest					# Fast and simple video download library and CLI tool written in Go
+    # github.com/shuLhan/go-bindata/...			# This package converts any file into managable Go source code. Useful for embedding binary data into a go program
 )
 
 for go_package in "${go_packages_to_install[@]}"; do
-    go get -u $go_package
+    go install $go_package
     echo -e "${go_package} installed\n"
 done
 
+# mkdir -p $HOME/go/bin
 cp ~/go/bin/* ~/bin/
