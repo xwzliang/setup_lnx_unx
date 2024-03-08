ce54bfb494bbd64cd426e731351478cd7e3b3ba5
Jolong <18706703437@163.com>
2024-03-08 16:45:45 +0800

    Create server_packages_mac_install.sh

---
+++ install_packages/server_packages_mac_install.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+command_exists() {
+    command -v "$@" > /dev/null 2>&1
+}
+
+# Install homebrew
+install_brew() {
+    if ! command_exists brew; then
+        echo -e "brew not installed, will install it\n"
+        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+    fi
+}
+
+install_brew
+
+# Install packages
+brew install jellyfin
+brew install --cask nextcloud
