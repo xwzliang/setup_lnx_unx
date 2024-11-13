9a5bc56d47ef18a46d5519110d8ac811845bd32f
broliang <18706703437@163.com>
2024-11-13 18:31:27 +0800

    Update install comfyui-manager.

--- setup_server_mac/comfyui/install.sh
+++ setup_server_mac/comfyui/install.sh
@@ -10,3 +10,9 @@ git clone https://github.com/comfyanonymous/ComfyUI.git
 cd ComfyUI
 
 pip install -r requirements.txt
+
+cd custom_nodes
+git clone https://github.com/ltdrdata/ComfyUI-Manager.git
+
+# cd ..
+# python main.py --listen 0.0.0.0
