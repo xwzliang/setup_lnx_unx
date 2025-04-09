3f69462beb129ff92e0f9aeaf8bfbf4b6d4602d1
broliang <18706703437@163.com>
2025-04-09 17:50:45 +0800

    minimal setup and conda

---
+++ minimal_setup.sh
@@ -0,0 +1,10 @@
+bash ./install_packages/conda_install.sh
+
+bash ./setup_packages/dir_setup.sh
+bash ./setup_packages/git_setup.sh
+
+cd ~/git
+
+git clone https://github.com/xwzliang/dotfiles.git
+
+bash ~/git/dotfiles/install.sh
